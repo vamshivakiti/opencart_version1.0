ff3ddf98d012c8a0038ab2ebc341aa50b165fd75
Vamshi Krishna <vamshivakiti@gmail.com>
2026-03-04 13:32:01 +0530

    03/04/2026

---
+++ src/test/java/seleniumPractice/DD_BootStrap.java
@@ -0,0 +1,5 @@
+package seleniumPractice;
+
+public class DD_BootStrap {
+
+}
--- src/test/java/seleniumPractice/DD_Select.java
+++ src/test/java/seleniumPractice/DD_Select.java
@@ -2,18 +2,24 @@ package seleniumPractice;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.time.Duration;
+import java.util.List;
 
 public class DD_Select {
     WebDriver driver;
     @BeforeTest
     public void beforeTest(){
-        driver= new ChromeDriver();
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--headless");
+        option.addArguments("--window-size=1920,1080");
+        driver= new ChromeDriver(option);
         driver.get("https://testautomationpractice.blogspot.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
@@ -26,6 +32,14 @@ public class DD_Select {
         Thread.sleep(3000);
         ddCount.selectByIndex(4);
         Thread.sleep(2000);
+      List<WebElement> options=  ddCount.getOptions();
+        System.out.println(options.size());
+    /*    for (int i=0;i<options.size();i++ ){
+            System.out.println(options.get(i).getText());
+        */
+        for(WebElement op:options){
+            System.out.println(op.getText());
+        }
         //ddCount.selectByValue("Japan");
         //Thread.sleep(2000);
         TakesScreenshot ts = (TakesScreenshot)driver;
@@ -33,7 +47,7 @@ public class DD_Select {
         File targetFile = new File(System.getProperty("user.dir")+"\\ScreenShotFolder\\ss.png");
         sourceFile.renameTo(targetFile);
     }
-
+@AfterTest
     public  void TearDown() throws InterruptedException {
         Thread.sleep(3000);
         driver.quit();
